7527822161833d9afa218c8fff4075dced3773d8
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-28 12:00:54 -0600

    fixed staticcontroller typo

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,9 @@ require 'rails/application_controller'
 
 class StaticController < Rails::ApplicationController
     layout false
+
   def index
-    render file: Rails.root.join('public, 'index.html')
+    render file: Rails.root.join('public', 'index.html')
   end
+
 end
